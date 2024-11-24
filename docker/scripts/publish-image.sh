5204ff50ccebdb5b06116dcd6bc20fe7819d38c1
Edudate Developer <devops@edudate.academy>
2024-11-23 19:51:16 -0800

    finalize scripting for building and publishing images

--- docker/scripts/publish-image.sh
+++ docker/scripts/publish-image.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# used to publish the current ALPHA image to docker hub as beta or shams (default is beta)
-
- if [ -z $1 ]; then  PUBLISH_ENV=alpha; else  PUBLISH_ENV=$1; fi
-./set-env.sh $1
+# used to publish [core|alpha] image to docker hub 
+BUILD=${1,,}
+if [ -z $BUILD ]; then BUILD=alpha; fi
+ if [ -z $BUILD ]; then  PUBLISH_ENV=alpha; else  PUBLISH_ENV=$BUILD; fi
+./set-env.sh $BUILD
 . .env
- cat ../secrets/docker-hahlabsdevops-access-token.txt | docker login --username hahlabsdevops --password-stdin
+ cat secrets/docker-hahlabsdevops-access-token.txt | docker login --username hahlabsdevops --password-stdin
  echo "Pushing image: "  $IMAGE_TAG:$PUBLISH_ENV-$RELEASE
  docker tag $ORG_ID/$IMAGE_NAME:$ENV $ORG_ID/$IMAGE_NAME:$PUBLISH_ENV-$RELEASE
  docker tag $ORG_ID/$IMAGE_NAME:$ENV $ORG_ID/$IMAGE_NAME:$PUBLISH_ENV
